6db70a03621e2f444fed6754f9900f465949431e
griffeyTheGr8 <42072301+griffeyTheGr8@users.noreply.github.com>
2019-11-20 09:51:53 -0600

    Grading Update #1
    Check the docstring for some things that you can work on.

--- 1.3.3 Mckinney.py
+++ 1.3.3 Mckinney.py
@@ -12,3 +12,16 @@ def hiLo_game(guess):
         print ("your number is too high")
     else:
         print ("your number is too low")
+        
+"""
+Documentation:
+1. Provide EOL comments of important items and describe their intended purpose using '#'
+2. Take out any additional EOL comments that are no longer necessay. 
+
+Spacing: 
+1. Provide appropriate spacing between lines to increase readability. 
+
+Coding:
+1. Need to assign a random number from 1 - 100 to the rand_num variable. 
+
+"""
